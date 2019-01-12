1af67b717c2c66444697b12c4031ca7bcb652b75
Riley X. Brady <riley.brady@colorado.edu>
2019-01-11 17:13:43 -0700

    setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+DISTNAME = 'esmtools'
+VERSION = '0.1'
+AUTHOR = 'Riley X. Brady'
+AUTHOR_EMAIL = 'riley.brady@colorado.edu'
+DESCRIPTION = 'A toolbox for functions related to Earth System Model analysis, with a focus on biogeochemical oceanography.'
+URL = 'https://github.com/bradyrx/esmtools'
+LICENSE = 'MIT'
+INSTALL_REQUIRES = ['xarray', 'pandas', 'numpy', 'matplotlib', 'xskillscore==0.0.2']
+DEPENDENCY_LINKS = ['https://github.com/raybellwaves/xskillscore/tarball/master#egg=xskillscore-0.0.2']
+# TODO: Add testing
+# TESTS_REQUIRE = ['pytest']
+PYTHON_REQUIRE = '>=3.6'
+
+setup(name=DISTNAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=open('README.md').read(),
+      url=URL,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      license=LICENSE,
+      packages=find_packages(),
+      # NOTE: This will be deprecated, so either need to move away from non-pypi packages or find another solution.
+      dependency_links=DEPENDENCY_LINKS,
+      # Needed for dependencies. Currently do not like the pyfinance or xskillscore dependency.
+      install_requires=INSTALL_REQUIRES,
+      python_requires=PYTHON_REQUIRE
+     )
