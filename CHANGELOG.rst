975b5ed8ac73b58a39d3f7978764d6be1740c2d6
Riley X. Brady <riley.brady@colorado.edu>
2019-07-25 15:13:59 -0600

    cleanup setup
    add versioning

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,13 @@
 Changelog History
 =================
 
+esmtools v1.0.1 (2019-07-25)
+============================
+
+Internals/Minor Fixes
+---------------------
+- Add versioning and clean up setup file.
+
 esmtools v1.0.0 (2019-07-25)
 ============================
 Formally releases ``esmtools`` on pip for easy installing by other packages.
--- esmtools/__init__.py
+++ esmtools/__init__.py
@@ -1,10 +1,21 @@
-from . import filtering
-from . import vis
-from . import stats
-from . import carbon
-from . import physics
-from . import conversions
-from . import mpas
-from . import loadutils
-from . import grid
-from . import composite
+from pkg_resources import DistributionNotFound, get_distribution
+
+from . import (
+    carbon,
+    composite,
+    conversions,
+    filtering,
+    grid,
+    loadutils,
+    mpas,
+    physics,
+    stats,
+    vis,
+)
+from .versioning.print_versions import show_versions
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
