d825db15fc44d9e57c009fb64d456584bbf7e9e1
Riley Brady <riley.brady@colorado.edu>
2019-08-30 12:17:44 -0600

    Update correlation to handle single time series (#58)
    * Add broadcasting to corr and negative lag
    
    * add compatible emissions to API on docs
    
    * remove old functions from API in docs
    
    * update test fixtures to be called multiple times

---
+++ docs/source/api/esm_analysis.carbon.plot_compatible_emissions.rst
@@ -0,0 +1,6 @@
+plot_compatible_emissions
+=========================
+
+.. currentmodule:: esm_analysis.carbon
+
+.. autofunction:: plot_compatible_emissions
--- esm_analysis/tests/test_stats.py
+++ esm_analysis/tests/test_stats.py
@@ -1,17 +1,79 @@
-import climpred
+import numpy as np
+import xarray as xr
+from esm_analysis.stats import corr
 import pytest
-from esm_analysis.stats import linear_regression
 
 
-@pytest.fixture
-def single_ts():
-    da = climpred.tutorial.load_dataset('FOSI-SST')
-    da = da['SST']
-    return da
+@pytest.fixture()
+def gridded_ts_da():
+    """Mock data of gridded time series."""
+    # Wrapper so fixture can be called multiple times.
+    # https://alysivji.github.io/pytest-fixures-with-function-arguments.html
+    def _gen_data():
+        data = np.random.rand(120, 10, 10)
+        da = xr.DataArray(data, dims=['time', 'lat', 'lon'])
+        # Monthly resolution time axis for 10 years.
+        da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
+        return da
+
+    return _gen_data
 
 
-def test_linear_regression(single_ts):
-    """Tests that linear regression works on a single time series."""
-    results = linear_regression(single_ts, dim='time')
-    for v in results.data_vars:
-        assert results[v]
+@pytest.fixture()
+def ts_monthly_da():
+    """Mock time series at monthly resolution for ten years."""
+    # Wrapper so fixture can be called multiple times.
+    # https://alysivji.github.io/pytest-fixures-with-function-arguments.html
+    def _gen_data():
+        data = np.random.rand(120)
+        da = xr.DataArray(data, dims=['time'])
+        # Monthly resolution time axis for 10 years.
+        da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
+        return da
+
+    return _gen_data
+
+
+def test_corr_two_grids(gridded_ts_da):
+    """Tests that correlations between two grids work."""
+    x = gridded_ts_da()
+    y = gridded_ts_da()
+    corrcoeff = corr(x, y, dim='time')
+    # check that there's no NaNs in the resulting output.
+    assert not corrcoeff.isnull().any()
+
+
+def test_corr_grid_and_ts(ts_monthly_da, gridded_ts_da):
+    """Tests that correlation works between a grid and a time series."""
+    x = ts_monthly_da()
+    y = gridded_ts_da()
+    corrcoeff = corr(x, y, dim='time')
+    # check that there's no NaNs in the resulting output.
+    assert not corrcoeff.isnull().any()
+
+
+def test_corr_lead(gridded_ts_da):
+    """Tests positive lead for correlation."""
+    x = gridded_ts_da()
+    y = gridded_ts_da()
+    corrcoeff = corr(x, y, dim='time', lead=3)
+    # check that there's no NaNs in the resulting output.
+    assert not corrcoeff.isnull().any()
+
+
+def test_corr_lag(gridded_ts_da):
+    """Tests negative lead for correlation."""
+    x = gridded_ts_da()
+    y = gridded_ts_da()
+    corrcoeff = corr(x, y, dim='time', lead=-3)
+    # check that there's no NaNs in the resulting output.
+    assert not corrcoeff.isnull().any()
+
+
+def test_corr_return_p(gridded_ts_da):
+    """Tests that p-value is returned properly for correlation."""
+    x = gridded_ts_da()
+    y = gridded_ts_da()
+    corrcoeff, pval = corr(x, y, dim='time', return_p=True)
+    # check that there's no NaNs in the resulting output.
+    assert not (corrcoeff.isnull().any()) & (pval.isnull().any())
