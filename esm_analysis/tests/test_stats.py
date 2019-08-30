06f027ddf90e8de9e9dd27b274ebff1ccce75571
Riley X. Brady <riley.brady@colorado.edu>
2019-08-30 11:11:59 -0700

    update test fixtures to be called multiple times

--- esm_analysis/tests/test_stats.py
+++ esm_analysis/tests/test_stats.py
@@ -1,27 +1,40 @@
 import numpy as np
 import xarray as xr
 from esm_analysis.stats import corr
+import pytest
 
 
+@pytest.fixture()
 def gridded_ts_da():
     """Mock data of gridded time series."""
-    data = np.random.rand(120, 10, 10)
-    da = xr.DataArray(data, dims=['time', 'lat', 'lon'])
-    # Monthly resolution time axis for 10 years.
-    da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
-    return da
+    # Wrapper so fixture can be called multiple times.
+    # https://alysivji.github.io/pytest-fixures-with-function-arguments.html
+    def _gen_data():
+        data = np.random.rand(120, 10, 10)
+        da = xr.DataArray(data, dims=['time', 'lat', 'lon'])
+        # Monthly resolution time axis for 10 years.
+        da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
+        return da
+
+    return _gen_data
 
 
+@pytest.fixture()
 def ts_monthly_da():
     """Mock time series at monthly resolution for ten years."""
-    data = np.random.rand(120)
-    da = xr.DataArray(data, dims=['time'])
-    # Monthly resolution time axis for 10 years.
-    da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
-    return da
+    # Wrapper so fixture can be called multiple times.
+    # https://alysivji.github.io/pytest-fixures-with-function-arguments.html
+    def _gen_data():
+        data = np.random.rand(120)
+        da = xr.DataArray(data, dims=['time'])
+        # Monthly resolution time axis for 10 years.
+        da['time'] = np.arange('1990-01', '2000-01', dtype='datetime64[M]')
+        return da
+
+    return _gen_data
 
 
-def test_corr_two_grids():
+def test_corr_two_grids(gridded_ts_da):
     """Tests that correlations between two grids work."""
     x = gridded_ts_da()
     y = gridded_ts_da()
@@ -30,7 +43,7 @@ def test_corr_two_grids():
     assert not corrcoeff.isnull().any()
 
 
-def test_corr_grid_and_ts():
+def test_corr_grid_and_ts(ts_monthly_da, gridded_ts_da):
     """Tests that correlation works between a grid and a time series."""
     x = ts_monthly_da()
     y = gridded_ts_da()
@@ -39,7 +52,7 @@ def test_corr_grid_and_ts():
     assert not corrcoeff.isnull().any()
 
 
-def test_corr_lead():
+def test_corr_lead(gridded_ts_da):
     """Tests positive lead for correlation."""
     x = gridded_ts_da()
     y = gridded_ts_da()
@@ -48,7 +61,7 @@ def test_corr_lead():
     assert not corrcoeff.isnull().any()
 
 
-def test_corr_lag():
+def test_corr_lag(gridded_ts_da):
     """Tests negative lead for correlation."""
     x = gridded_ts_da()
     y = gridded_ts_da()
@@ -57,7 +70,7 @@ def test_corr_lag():
     assert not corrcoeff.isnull().any()
 
 
-def test_corr_return_p():
+def test_corr_return_p(gridded_ts_da):
     """Tests that p-value is returned properly for correlation."""
     x = gridded_ts_da()
     y = gridded_ts_da()
