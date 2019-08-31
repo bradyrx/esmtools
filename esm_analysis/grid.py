9910c79c1b2b3d1012a0efc0cb337b7ecc909967
Riley X. Brady <riley.brady@colorado.edu>
2019-08-30 21:11:32 -0600

    account for 2D lat/lon grids
    add docstrings to grid functions
    
    create generalized convert_lon function
    
    add docstring for general convert_lon function
    
    Add CoordinateError to custom exceptions

--- esm_analysis/grid.py
+++ esm_analysis/grid.py
@@ -1,28 +1,85 @@
+from .exceptions import CoordinateError
 from .utils import check_xarray
 
+# NOTE: Add testing
+# NOTE: Compile docs for new API. Delete old functions.
+
 
 @check_xarray(0)
-def convert_lon_to_180to180(ds, lon_coord='lon'):
-    """Convert from 0 to 360 grid to -180 to 180 grid."""
+def _convert_lon_to_180to180(ds, coord='lon'):
+    """Convert from 0 to 360 (degrees E) grid to -180 to 180 (W-E) grid.
+
+    .. note::
+        Longitudes are not sorted after conversion (i.e., spanning -180 to 180 or
+        0 to 360 from index 0, ..., N), as it is expected that the user will plot
+        via ``cartopy``, ``basemap``, or ``xarray`` plot functions.
+
+    Args:
+        ds (xarray object): Dataset to be converted.
+        coord (optional str): Name of longitude coordinate.
+
+    Returns:
+        xarray object: Dataset with converted longitude grid.
+    """
     ds = ds.copy()
-    lon = ds[lon_coord].values
+    lon = ds[coord].values
+    # Convert everything over 180 back to the negative (degrees W) values.
     lon[lon > 180] = lon[lon > 180] - 360
-    ds.coords[lon_coord] = lon
-    ds = ds.sortby(lon_coord)
+    # Need to account for clarifying dimensions if the grid is 2D.
+    ds.coords[coord] = (ds[coord].dims, lon)
     return ds
 
 
 @check_xarray(0)
-def convert_lon_to_0to360(ds, lon_coord='lon'):
-    """Convert from -180 to 180 grid to 0 to 360 grid."""
+def _convert_lon_to_0to360(ds, coord='lon'):
+    """Convert from -180 to 180 (W-E) to 0 to 360 (degrees E) grid.
+
+    .. note::
+        Longitudes are not sorted after conversion (i.e., spanning -180 to 180 or
+        0 to 360 from index 0, ..., N), as it is expected that the user will plot
+        via ``cartopy``, ``basemap``, or ``xarray`` plot functions.
+
+    Args:
+        ds (xarray object): Dataset to be converted.
+        coord (optional str): Name of longitude coordinate.
+
+    Returns:
+        xarray object: Dataset with converted longitude grid.
+    """
     ds = ds.copy()
-    lon = ds[lon_coord].values
+    lon = ds[coord].values
+    # Convert -180 to 0 into scale reaching 360.
     lon[lon < 0] = lon[lon < 0] + 360
-    ds.coords[lon_coord] = lon
-    ds = ds.sortby(lon_coord)
+    # Need to account for clarifying dimensions if the grid is 2D.
+    ds.coords[coord] = (ds[coord].dims, lon)
     return ds
 
 
+@check_xarray(0)
 def convert_lon(ds, coord='lon'):
-    """Will infer which longitude grid it's on"""
-    pass
+    """Converts longitude grid from -180to180 to 0to360 and vice versa.
+
+    .. note::
+        Longitudes are not sorted after conversion (i.e., spanning -180 to 180 or
+        0 to 360 from index 0, ..., N), as it is expected that the user will plot
+        via ``cartopy``, ``basemap``, or ``xarray`` plot functions.
+
+    Args:
+        ds (xarray object): Dataset to be converted.
+        coord (optional str): Name of longitude coordinate.
+
+    Returns:
+        xarray object: Dataset with converted longitude grid.
+
+    Raises:
+        CoordinateError: If ``coord`` does not exist in the dataset.
+    """
+    if coord not in ds.coords:
+        raise CoordinateError(f'{coord} not found in coordinates.')
+    # NOTE: Check weird POP grid that Eleanor sent. Could have a min less than 0
+    # and max greater than 180. Could just throw error then.
+    if ds[coord].min() < 0:
+        ds = _convert_lon_to_0to360(ds, coord=coord)
+    else:
+        ds = _convert_lon_to_180to180(ds, coord=coord)
+    return ds
