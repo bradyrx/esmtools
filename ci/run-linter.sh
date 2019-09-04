ee8b671bebbe9f71f574f072868f1f2616897506
Riley X. Brady <riley.brady@colorado.edu>
2019-09-04 08:17:10 -0600

    change ci name to esmtools

--- ci/run-linter.sh
+++ ci/run-linter.sh
@@ -4,10 +4,10 @@ set -eo pipefail
 
 echo "Code Styling with (black, flake8, isort)"
 
-source activate esm-analysis-dev
+source activate esmtools-dev
 
 echo "[flake8]"
 flake8 esm_analysis --max-line-length=88 --exclude=__init__.py --ignore=W605,W503
 
 echo "[black]"
-black --check --line-length=88 -S esm_analysis 
+black --check --line-length=88 -S esmtools
