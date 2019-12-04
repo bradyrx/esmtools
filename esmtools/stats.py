ba0834bd51fdd23d2001ec2d51fe0565545dfe7d
Riley Brady <riley.brady@colorado.edu>
2019-12-04 15:49:49 -0700

    finish compute_slopes dask-compatible
    reorganize ci env

--- esmtools/stats.py
+++ esmtools/stats.py
@@ -5,6 +5,7 @@ import xarray as xr
 from scipy.stats import linregress as lreg
 
 from .checks import is_xarray, get_dims, has_dims
+from .utils import convert_time
 
 
 # ------------------
@@ -137,7 +138,7 @@ def area_weight(da, area_coord="area"):
 # -----------
 # TIME SERIES
 # -----------
-@is_xarray(0)
+@is_xarray([0, 1])
 def compute_slope(x, y, dim="time"):
     """Returns the linear slope with y regressed onto x.
 
@@ -149,10 +150,62 @@ def compute_slope(x, y, dim="time"):
 
     Returns:
         xarray object: Slopes computed through a least-squares linear regression.
+
+    TO TEST:
+    * DataArray (grid/ts) works
+    * Dataset (grid/ts) works
+    * Dask works
     """
-    # Return with attributes appended to it? datetime, package version, link to docs.
-    # Maybe also a note on units (try to pull .attrs['units'] and add per day to end)
-    pass
+
+    def _compute_slope(x, y):
+        """Private function to wrap polyfit.
+
+        Args:
+            x (ndarray): Independent variable for linear regression.
+            y (ndarray): Dependent variable for linear regression.
+        """
+        return np.polyfit(x, y, 1)[0]  # return only the slope.
+
+    # Converts time to days since 1990 to regress accounting for differences
+    # in e.g. lengths of months.
+    if x.name == dim:
+        x = convert_time(x, dim)
+    if y.name == dim:
+        y = convert_time(y, dim)
+
+    # NOTE: Add NaN control functionality. Will have subfunction but should
+    # skip, interpoalte, etc.
+
+    slopes = xr.apply_ufunc(
+        _compute_slope,
+        x,
+        y,
+        vectorize=True,
+        dask="parallelized",
+        input_core_dims=[[dim], [dim]],
+        output_dtypes=[float],
+    )
+    return slopes
+
+
+@is_xarray([0, 1])
+def linear_regression(x, y, dim="time"):
+    """Applies the `scipy.stats` linregress to a grid."""
+    x = convert_time(x, dim)
+    # NOTE: Add NaN control.
+    # NOTE: Can't use `parallelized` with multiple outputs. Either look
+    # into `map_blocks` or think about other ways to get significance.
+    # Is it just significance you want here?
+    results = xr.apply_ufunc(
+        lreg,
+        x,
+        y,
+        input_core_dims=[[dim], [dim]],
+        output_core_dims=[[], [], [], [], []],
+        vectorize=True,
+        dask="allowed",
+    )
+    return results
 
 
 @is_xarray(0)
@@ -245,121 +298,6 @@ def fit_poly(ds, order, dim="time"):
         )
     else:
         return fit
-
-
-@is_xarray(0)
-def linear_regression(da, dim="time", interpolate_na=False, compact=True, psig=None):
-    """
-    Computes the least-squares linear regression of an xr.DataArray x against
-    another xr.DataArray y.
-
-    Note: This only returns the statistics from the LSR (slope, intercept, rvalue,
-    pvalue, stderr). Use `fit_poly` to get the fitted line returned.
-
-    Parameters
-    ----------
-    da : xarray DataArray
-    dim : str (default to 'time')
-        dimension over which to compute the linear regression.
-    interpolate_na : bool (default to False)
-        If True, linearly interpolate NaNs (and backfill/forwardfill). Note that if
-        this is False, the linear regression will return NaN if there are *any*
-        NaNs in the time series.
-    compact : boolean (default to True)
-        If true, return all results of linregress as a single dataset.
-        If false, return results as five separate DataArrays.
-    psig : double (default to None)
-        Alpha level for correlation significance. If not None, NaNs out any grid cells
-        that are larger than psig.
-
-    Returns
-    -------
-    ds : xarray Dataset
-        Dataset containing slope, intercept, rvalue, pvalue, stderr from
-        the linear regression. Excludes the dimension the regression was
-        computed over. If compact is False, these five parameters are
-        returned separately.
-    """
-    # Check if dataset.
-    if isinstance(da, xr.Dataset):
-        raise NotImplementedError(
-            "Datasets are not yet supported for this function. "
-            + "Please retry with a DataArray."
-        )
-
-    x = da[dim]
-    # Linear regression doesn't like to work with actual datetime. So temporarily
-    # convert to integers.
-    # `np.object` covers cftime.
-    if np.issubdtype(x, np.datetime64) or isinstance(x, np.object):
-        x = np.arange(len(x))
-
-    if interpolate_na:
-        # borrowed from @ahuang11's implementation in `climpred`
-        da_dims_orig = list(da.dims)  # orig -> original
-        if len(da_dims_orig) > 1:
-            # want independent axis to be the leading dimension
-            da_dims_swap = da_dims_orig.copy()  # copy to prevent contamination
-
-            # https://stackoverflow.com/questions/1014523/
-            # simple-syntax-for-bringing-a-list-element-to-the-front-in-python
-            da_dims_swap.insert(0, da_dims_swap.pop(da_dims_swap.index(dim)))
-            da = da.transpose(*da_dims_swap)
-
-            # hide other dims into a single dim
-            da = da.stack({"other_dims": da_dims_swap[1:]})
-            dims_swapped = True
-        else:
-            dims_swapped = False
-
-        # This is borrowed from @ahuang11's implementation in `climpred` to handle
-        # NaNs in the time series.
-        nan_locs = np.isnan(da.values)
-
-        # any(nan_locs.sum(axis=0)) fails if not 2D
-        if nan_locs.ndim == 1:
-            nan_locs = nan_locs.reshape(len(nan_locs), 1)
-
-        # check if there's any NaNs in the provided dim because
-        # interpolate_na is computationally expensive to run regardless of NaNs
-        # if nan_locs.sum(dim=dim).any():
-        if any(nan_locs.sum(axis=0)) > 0:
-            da = da.interpolate_na(dim)
-            if any(nan_locs[0, :]):
-                # [np.nan, 1, 2], no first value to interpolate from; back fill
-                da = da.bfill(dim)
-            if any(nan_locs[-1, :]):
-                # [0, 1, np.nan], no last value to interpolate from; forward fill
-                da = da.ffill(dim)
-
-        # this handles the other axes; doesn't matter since it won't affect the fit
-        da = da.fillna(0)
-        if dims_swapped:
-            da = da.unstack("other_dims").transpose(*da_dims_orig)
-
-    results = xr.apply_ufunc(
-        lreg,
-        x,
-        da,
-        input_core_dims=[[dim], [dim]],
-        output_core_dims=[[], [], [], [], []],
-        vectorize=True,
-        dask="allowed",
-    )
-
-    # Force into a cleaner dataset. The above function returns a dataset
-    # with no clear labeling.
-    ds = xr.Dataset()
-    labels = ["slope", "intercept", "rvalue", "pvalue", "stderr"]
-    for i, l in enumerate(labels):
-        results[i].name = l
-        ds = xr.merge([ds, results[i]])
-    if psig is not None:
-        ds = ds.where(ds["pvalue"] < psig)
-    if compact:
-        return ds
-    else:
-        return ds["slope"], ds["intercept"], ds["rvalue"], ds["pvalue"], ds["stderr"]
 
 
 @is_xarray(0)
