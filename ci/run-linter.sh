1a144edc8681e2b7ea77ca6d587537f7cf671920
Riley Brady <riley.brady@colorado.edu>
2020-07-08 14:31:08 -0600

    fix linter?

--- ci/run-linter.sh
+++ ci/run-linter.sh
@@ -7,12 +7,10 @@ echo "Code Styling with (black, flake8, isort)"
 source activate esmtools-dev
 
 echo "[flake8]"
-# NOTE: F401 should be removed if we can figure out how to import an accessor
-# created within the package.
 flake8 esmtools --max-line-length=88 --exclude=__init__.py --ignore=W605,W503,F722,C901,F401
 
 echo "[black]"
 black --check --line-length=88 -S esmtools
 
 echo "[isort]"
-isort --recursive -w 88 --check-only esmtools
+isort -w 88 --check-only esmtools
